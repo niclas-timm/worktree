5bd032c20af4cee7941c4a81b309222e2d34158d
Niclas Timm <niclastimmdev@gmail.com>
2025-12-30 12:44:47 +0100

    Configure DB

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,8 +134,12 @@ WSGI_APPLICATION = 'backend.config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'ticketing'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5430'),
     }
 }
 
